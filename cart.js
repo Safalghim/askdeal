5a737540ceae074d790f124b1b8fa8c7b540fb0a
Hari Thapa <H.thapa2@wlv.ac.uk>
2025-05-04 03:33:27 +0100

    cart.js

--- cart.js
+++ cart.js
@@ -1,3 +1,5 @@
+// cart.js
+
 // Function to add a product to the cart
 function addToCart(productName, productPrice, imgSrc) {
     // Retrieve the cart from localStorage or initialize an empty array
